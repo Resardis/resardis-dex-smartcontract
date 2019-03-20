44e9b901497d89f4bc631b44096bbbaf20e479ea
Cagatay Aydin <aydincagatay@yandex.com>
2019-03-20 16:54:28 +0300

    Added migrations file for dex, inc. constructor variables.

---
+++ migrations/2_dex_migration.js
@@ -0,0 +1,12 @@
+const Resardis = artifacts.require("Resardis");
+
+module.exports = function(deployer, network, accounts) {
+  const admin = accounts[0];
+  const feeAccount = accounts[1];
+  const accountLevelsAddr = accounts[9];
+  const feeMake = web3.utils.toBN(web3.utils.toWei('0.002', 'ether'));
+  const feeTake = web3.utils.toBN(web3.utils.toWei('0.002', 'ether'));
+  const feeRebate = web3.utils.toBN(web3.utils.toWei('0.002', 'ether'));
+  const noFeeUntil = 1765497600;
+  deployer.deploy(Resardis, admin, feeAccount, accountLevelsAddr, feeMake, feeTake, feeRebate, noFeeUntil);
+};
