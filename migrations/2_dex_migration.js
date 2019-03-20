a859fa16b6b26308035058d85a7cb645c38104e7
Cagatay Aydin <aydincagatay@yandex.com>
2019-03-20 18:00:12 +0300

    Convert UNIX timestamp to BN/BigNumber before inputting.

--- migrations/2_dex_migration.js
+++ migrations/2_dex_migration.js
@@ -8,7 +8,7 @@ module.exports = function(deployer, network, accounts) {
     const feeMake = web3.utils.toBN(web3.utils.toWei('0.002', 'ether'));
     const feeTake = web3.utils.toBN(web3.utils.toWei('0.002', 'ether'));
     const feeRebate = web3.utils.toBN(web3.utils.toWei('0.002', 'ether'));
-    const noFeeUntil = 1765497600;
+    const noFeeUntil = web3.utils.toBN(1765497600);
     deployer.deploy(Resardis, admin, feeAccount, accountLevelsAddr, feeMake, feeTake, feeRebate, noFeeUntil);
   }
 };
