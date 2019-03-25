5cc74485436100044e7ef5312986abae1432b0ad
Cagatay Aydin <aydincagatay@yandex.com>
2019-03-25 19:44:48 +0300

    Deploy mintable ERC20 instead of the plain ERC20.

--- migrations/3_erc20_migration.js
+++ migrations/3_erc20_migration.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const erc20 = artifacts.require('ERC20');
+const erc20 = artifacts.require('ERC20Mintable');
 
 module.exports = function (deployer, network, accounts) {
   if (network == 'development') { // eslint-disable-line
