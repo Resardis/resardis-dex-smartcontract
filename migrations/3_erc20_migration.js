f665bbf1bf41068eb1107b56d97b2a1d2dcab18a
Cagatay Aydin <aydincagatay@yandex.com>
2019-11-15 18:56:55 +0300

    Moved all erc token migrations to single file. Added extdev and rinkeby.

--- migrations/3_erc20_migration.js
+++ migrations/3_erc20_migration.js
@@ -1,9 +1,21 @@
 'use strict';
 
-const erc20 = artifacts.require('ERC20Mintable');
+const erc20 = artifacts.require('ERC20MintableX');
+const resardistoken = artifacts.require('ERC20MintableY');
+const erc20Loom = artifacts.require('ERC20Loom');
 
-module.exports = function (deployer, network, accounts) {
-  if (network == 'ganache_local') { // eslint-disable-line
-    deployer.deploy(erc20);
+const gatewayAddress = '0xe754d9518bf4a9c63476891ef9AA7d91C8236A5D';
+
+module.exports = async (deployer, network, accounts) => {
+  if (network === 'ganache_local') {
+    await deployer.deploy(erc20);
+    await deployer.deploy(resardistoken);
+  } else if (network === 'rinkeby') {
+    const mintAmount = web3.utils.toBN(web3.utils.toWei('1000', 'ether'));
+    await deployer.deploy(erc20);
+    const tokenInstance = await erc20.deployed();
+    await tokenInstance.mint(accounts[0], mintAmount);
+  } else if (network === 'loom_extdev_plasma_us1') {
+    await deployer.deploy(erc20Loom, gatewayAddress);
   }
 };
