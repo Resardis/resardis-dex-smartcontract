355834c7f5e39a32ca33e12cec9c29f5dcc19126
Cagatay Aydin <aydincagatay@yandex.com>
2019-11-17 22:43:06 +0300

    Bump solc to 0.5.13, use petersburg EVM, disable the optimizer.

--- truffle-config.js
+++ truffle-config.js
@@ -175,14 +175,14 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: '0.5.0', // Fetch exact version from solc-bin (default: truffle's version)
-      docker: false, // Use "0.5.1" you've installed locally with docker (default: false)
+      version: '0.5.13', // also check evmVersion for compatibility
+      docker: false, // Use "x.x.x" you've installed locally with docker (default: false)
       settings: { // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-          enabled: true,
-          runs: 1000,
+          enabled: false,
+          // runs: 1000,
         },
-        evmVersion: 'byzantium',
+        evmVersion: 'petersburg', // also check solc version for compatibility
       },
     },
   },
