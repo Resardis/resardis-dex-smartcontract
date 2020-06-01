4f864873cadfdd6202dc0204b2c5bc5ca576e002
Cagatay Aydin <aydincagatay@yandex.com>
2020-06-01 14:18:28 +0300

    Removed extdev migrations.

--- migrations/2_dex_migration.js
+++ migrations/2_dex_migration.js
@@ -3,11 +3,11 @@
 const Resardis = artifacts.require('Resardis');
 
 module.exports = function (deployer, network, accounts) {
-  if (network === 'ganache_local' || network === 'loom_extdev_plasma_us1') {
+  if (network === 'ganache_local') {
     const admin = accounts[0];
     const feeAccount = accounts[0];
-    const feeMake = web3.utils.toBN(web3.utils.toWei('0.00015', 'ether'));
-    const feeTake = web3.utils.toBN(web3.utils.toWei('0.00025', 'ether'));
+    const feeMake = web3.utils.toBN(web3.utils.toWei('0.0015', 'ether'));
+    const feeTake = web3.utils.toBN(web3.utils.toWei('0.0025', 'ether'));
     const resardisTokenFee = web3.utils.toBN(web3.utils.toWei('0.47', 'ether'));
     const noFeeUntil = web3.utils.toBN(1765497600);
     deployer.deploy(Resardis, admin, feeAccount, feeMake, feeTake, resardisTokenFee, noFeeUntil);
