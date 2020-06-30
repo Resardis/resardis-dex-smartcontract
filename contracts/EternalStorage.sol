da9b15e00ee89aa8fefa539ae63b8bdc0dbcbdff
Cagatay Aydin <aydincagatay@yandex.com>
2020-06-30 19:30:33 +0300

    Moved deposit/withdraw functions to eternal storage.

--- contracts/EternalStorage.sol
+++ contracts/EternalStorage.sol
@@ -25,4 +25,55 @@ contract EternalStorage {
     //mapping of token addresses to permission.
     //If true, token is allowed to be withdrawed.
     mapping (address => bool) public allowedWithdrawTokens;
+
+    function deposit() external payable {
+        tokens[address(0)][msg.sender] = tokens[address(0)][msg.sender].add(msg.value);
+        emit Deposit(
+            address(0),
+            msg.sender,
+            msg.value,
+            tokens[address(0)][msg.sender]
+        );
+    }
+
+    function withdraw(uint amount) external {
+        require(tokens[address(0)][msg.sender] >= amount);
+        tokens[address(0)][msg.sender] = tokens[address(0)][msg.sender].sub(amount);
+        msg.sender.transfer(amount);
+        emit Withdraw(
+            address(0),
+            msg.sender,
+            amount,
+            tokens[address(0)][msg.sender]
+        );
+    }
+
+    function depositToken(address token, uint amount) external {
+        //remember to call Token(address).approve(this, amount)
+        //or this contract will not be able to do the transfer on your behalf.
+        require(token!=address(0));
+        require(allowedDepositTokens[token] == true);
+        tokens[token][msg.sender] = tokens[token][msg.sender].add(amount);
+        require(IERC20(token).transferFrom(msg.sender, address(this), amount));
+        emit Deposit(
+            token,
+            msg.sender,
+            amount,
+            tokens[token][msg.sender]
+        );
+    }
+
+    function withdrawToken(address token, uint amount) external {
+        require(token!=address(0));
+        require(allowedWithdrawTokens[token] == true);
+        require(tokens[token][msg.sender] >= amount);
+        tokens[token][msg.sender] = tokens[token][msg.sender].sub(amount);
+        require(IERC20(token).transfer(msg.sender, amount));
+        emit Withdraw(
+            token,
+            msg.sender,
+            amount,
+            tokens[token][msg.sender]
+        );
+    }
 }
