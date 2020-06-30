c14877ccfef7244e8081f0e27394f8ed12643875
Cagatay Aydin <aydincagatay@yandex.com>
2020-06-30 18:54:10 +0300

    Import new contracts. Separate state vars.

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -1,35 +1,16 @@
 pragma solidity ^0.5.0;
 
-import "./vendor/SafeMath.sol";
-import "./vendor/IERC20.sol";
+import "./vendor/openzeppelin/SafeMath.sol";
+import "./vendor/openzeppelin/IERC20.sol";
+
+import "./EternalStorage.sol";
+
+import "./market/MarketBase.sol";
+import "./market/MarketExpiring.sol";
+import "./market/MarketMatching.sol";
 
 contract Resardis {
     using SafeMath for uint256;
-
-    address public admin; //the admin address
-    address public feeAccount; //the account that will receive fees
-    address public resardisToken;
-    uint public feeMake; //percentage times (1 ether)
-    uint public feeTake; //percentage times (1 ether)
-    uint public noFeeUntil; // UNIX timestamp, no fee charged until that time
-    uint public resardisTokenFee;
-    //mapping of token addresses to mapping of account balances (token=0 means Ether)
-    mapping (address => mapping (address => uint)) public tokens;
-    //mapping of user accounts to mapping of order hashes to booleans
-    //true = submitted by user, equivalent to offchain signature)
-    mapping (address => mapping (bytes32 => bool)) public orders;
-    //mapping of user accounts to mapping of order hashes to uints
-    //amount of order that has been filled)
-    mapping (address => mapping (bytes32 => uint)) public orderFills;
-    //mapping of user accounts to mapping of fee payment option
-    //0 = pays ether as a fee, 1 = pays resardiscoin as a fee.
-    mapping (address => bool) public feeOption;
-    //mapping of token addresses to permission.
-    //If true, token is allowed to be deposited/traded/ordered.
-    mapping (address => bool) public allowedDepositTokens;
-    //mapping of token addresses to permission.
-    //If true, token is allowed to be withdrawed.
-    mapping (address => bool) public allowedWithdrawTokens;
 
     event Order(
         address tokenGet,
