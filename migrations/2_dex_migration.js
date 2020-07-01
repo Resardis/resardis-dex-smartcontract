d53e9870b46c697ae82412e94890dbe1a45c8915
Cagatay Aydin <aydincagatay@yandex.com>
2020-07-01 18:51:59 +0300

    Migration for importer contract.

--- migrations/2_dex_migration.js
+++ migrations/2_dex_migration.js
@@ -4,12 +4,18 @@ const Resardis = artifacts.require('Resardis');
 
 module.exports = function (deployer, network, accounts) {
   if (network === 'ganache_local') {
-    const admin = accounts[0];
-    const feeAccount = accounts[0];
-    const feeMake = web3.utils.toBN(web3.utils.toWei('0.0015', 'ether'));
-    const feeTake = web3.utils.toBN(web3.utils.toWei('0.0025', 'ether'));
-    const resardisTokenFee = web3.utils.toBN(web3.utils.toWei('0.47', 'ether'));
-    const noFeeUntil = web3.utils.toBN(1765497600);
-    deployer.deploy(Resardis, admin, feeAccount, feeMake, feeTake, resardisTokenFee, noFeeUntil);
+    deployer.deploy(Resardis);
   }
 };
+
+// module.exports = function (deployer, network, accounts) {
+//   if (network === 'ganache_local') {
+//     const admin = accounts[0];
+//     const feeAccount = accounts[0];
+//     const feeMake = web3.utils.toBN(web3.utils.toWei('0.0015', 'ether'));
+//     const feeTake = web3.utils.toBN(web3.utils.toWei('0.0025', 'ether'));
+//     const resardisTokenFee = web3.utils.toBN(web3.utils.toWei('0.47', 'ether'));
+//     const noFeeUntil = web3.utils.toBN(1765497600);
+//     deployer.deploy(Resardis, admin, feeAccount, feeMake, feeTake, resardisTokenFee, noFeeUntil);
+//   }
+// };
