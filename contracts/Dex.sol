12a5d4e8890d852a7dbc9121113a5291f89849c9
Cagatay Aydin <aydincagatay@yandex.com>
2020-07-08 01:57:00 +0300

    Cleanup.

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -5,21 +5,11 @@ import "./market/MarketMatching.sol";
 contract Resardis is MatchingMarket {
     constructor(
         address admin_,
-        address feeAccount_,
-        uint feeMake_,
-        uint feeTake_,
-        uint resardisTokenFee_,
-        uint noFeeUntil_,
         uint64 _close_time
     )
         public
     {
         admin = admin_;
-        feeAccount = feeAccount_;
-        feeMake = feeMake_;
-        feeTake = feeTake_;
-        resardisTokenFee = resardisTokenFee_;
-        noFeeUntil = noFeeUntil_;
         close_time = _close_time;
     }
 }
--- migrations/2_dex_migration.js
+++ migrations/2_dex_migration.js
@@ -11,12 +11,7 @@ const Resardis = artifacts.require('Resardis');
 module.exports = function (deployer, network, accounts) {
   if (network === 'ganache_local') {
     const admin = accounts[0];
-    const feeAccount = accounts[0];
-    const feeMake = web3.utils.toBN(web3.utils.toWei('0.0015', 'ether'));
-    const feeTake = web3.utils.toBN(web3.utils.toWei('0.0025', 'ether'));
-    const resardisTokenFee = web3.utils.toBN(web3.utils.toWei('0.47', 'ether'));
-    const noFeeUntil = web3.utils.toBN(1765497600);
     const close_time = web3.utils.toBN(2145916800); // 2038
-    deployer.deploy(Resardis, admin, feeAccount, feeMake, feeTake, resardisTokenFee, noFeeUntil, close_time);
+    deployer.deploy(Resardis, admin, close_time);
   }
 };
