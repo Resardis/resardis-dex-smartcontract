62258205d1725d81abddd2931bfa22dbe11b4362
Cagatay Aydin <aydincagatay@yandex.com>
2020-07-08 20:20:27 +0300

    Bump solc to 0.5.17.

--- truffle-config.js
+++ truffle-config.js
@@ -114,7 +114,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: '0.5.13', // also check evmVersion for compatibility
+      version: '0.5.17', // also check evmVersion for compatibility
       docker: false, // Use "x.x.x" you've installed locally with docker (default: false)
       settings: { // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
