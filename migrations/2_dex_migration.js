ad5a5b66a77e1df6e9737d96de2868f085a13ffe
Cagatay Aydin <aydincagatay@yandex.com>
2020-07-28 16:28:59 +0300

    Populate with dummy data if deployed to matic testnet.

--- migrations/2_dex_migration.js
+++ migrations/2_dex_migration.js
@@ -2,10 +2,47 @@
 
 const Resardis = artifacts.require('Resardis');
 
-module.exports = function (deployer, network, accounts) {
-  if (network === 'ganache_local' || network === 'matic_testnet') {
+module.exports = async (deployer, network, accounts) => {
+  if (network === 'ganache_local') {
     const admin = accounts[0];
     console.log("Admin Account Address: " + admin.toString());
     deployer.deploy(Resardis, admin);
+  }
+  else if (network === 'matic_testnet') {
+    const admin = accounts[0];
+    const secAccount = accounts[1];
+
+    console.log("Admin Account Address: " + admin.toString());
+    console.log("Secondary Account Address: " + secAccount.toString());
+
+    const addressZero = web3.utils.toChecksumAddress('0x0000000000000000000000000000000000000000');
+    const tokenAddress = web3.utils.toChecksumAddress('0x2d7882beDcbfDDce29Ba99965dd3cdF7fcB10A1e'); // Matic Test ERC30
+
+    const depAmountEth = web3.utils.toBN(web3.utils.toWei('0.35', 'ether'));
+    let amountGet = web3.utils.toBN(web3.utils.toWei('0.0020', 'ether'));
+    let amountGive = web3.utils.toBN(web3.utils.toWei('0.0013', 'ether'));
+
+    await deployer.deploy(Resardis, admin);
+    const dexInstance = await Resardis.deployed();
+
+    // Allow Limit Order
+    await dexInstance.changeAvailableOfferType(0, true, { from: admin });
+    // Allow Market Order
+    await dexInstance.changeAvailableOfferType(1, true, { from: admin });
+    // Deposit some ETH
+    await dexInstance.deposit({ from: secAccount, value: depAmountEth });
+
+    // Fill with dummy Limit orders
+    let i;
+    for (i = 0; i < 10; i++) {
+      amountGet = amountGet.add(web3.utils.toBN(web3.utils.toWei('0.0003', 'ether')));
+      amountGive = amountGive.add(web3.utils.toBN(web3.utils.toWei('0.0002', 'ether')));
+
+      await dexInstance.offer(
+        amountGive, addressZero, amountGet, tokenAddress, 0, true, 0,
+        { from: secAccount, value: 0 },
+      );
+    }
+
   }
 };
